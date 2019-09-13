672238c0977ad40c14c21c497ccf5875a9321034
sonniakemmer <brownbugz@gmail.com>
2019-09-12 21:38:58 -0700

    adds ProjectUpdate and UpdateView

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Project
 
 class ProjectCreate(CreateView):
     model = Project
     fields = ['user', 'profile_link', 'github_link', 'about_me']
 
+class ProjectUpdate(UpdateView):
+    model = Project
+    fields = ['github_link', 'about_me']
 
 def home(request):
     return render(request, 'home.html')
