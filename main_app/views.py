b7a6c8717dd7beec00a1d9f6942e8ac16a461b22
sonniakemmer <brownbugz@gmail.com>
2019-09-12 21:31:21 -0700

    adds CreateView and ProjectCreate

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .models import Project
+
+class ProjectCreate(CreateView):
+    model = Project
+    fields = ['user', 'profile_link', 'github_link', 'about_me']
+
 
 def home(request):
     return render(request, 'home.html')
